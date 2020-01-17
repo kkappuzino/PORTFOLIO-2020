03bb387bc670eda771f0271cbe738df4ed463ac4
Henna <henna@mediaporras.com>
2020-01-17 15:04:39 +0200

    trying

---
+++ Structure/home.php
@@ -0,0 +1,51 @@
+<section id="home">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 v-center">
+                <div class="neon-line main">
+                    <div class="title-main">
+                        <h1 class="">VENLA PAANILA</h1>
+                        <h2 class=""> / DEVELOPER</h2>
+                        <h2 class=""> / ARTIST</h2>
+                    </div> 
+                </div>
+    
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="skills">
+    <div class="row">
+        <div class="col-12 v-center">
+            <div class="neon-line skills">
+                <div class="title-skills">
+                    <h1 class="">SKILLS</h1>
+                </div> 
+            </div>
+        </div><br>
+<!--        <div class="col-12 col-md-2 up">
+            <ul>
+                <li class="skills"><h3><i class="far fa-check-square"></i>  SCSS</h3></li>
+                <li class="skills"><h3><i class="far fa-check-square"></i>  Wordpress</h3></li>
+                <li class="skills"><h3><i class="far fa-check-square"></i>  Java</h3></li>
+                <li class="skills"><h3><i class="far fa-check-square"></i>  Gitbash</h3></li>
+                <li class="skills"><h3><i class="far fa-check-square"></i>  less</h3></li>
+                <li class="skills"><h3><i class="far fa-check-square"></i>  Sql</h3></li>
+                <li class="skills"><h3><i class="far fa-check-square"></i>  Office 365</h3></li>
+                <li class="skills"><h3><i class="far fa-check-square"></i>  Bootstrap</h3></li>
+            </ul>
+        </div>-->
+        <div class="col-12 col-sm-12 col-md-6 up">
+            <h2>FrontEnd</h2>
+            <p>I like finding new ways to make a webpage look unique. For this purpouse I tend to use javascript or jQuery with SCSS to always have something unique on the page. I also like to use different frameworks like Bootstrap to ease the process and focus on the outlook.</p>
+            <h2>BackEnd</h2>
+            <p>For BackEnd i tend to use PHP, but I'm also capable of using other languages and I'm always up for learning something new.</p>
+            <h2>Making games</h2>
+            <p>The moment that we started making games in class I fell in love. Unity with C# is the dream, coding C# came so naturally and it's such an easy language to write. That's why I started making my own game, in hopes to make it the best game I could ever make.</p>
+        </div>
+    </div>
+</section>
+<section id="art">
+
+</section>
---
+++ head.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Venlan CV</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="manifest" href="./img/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="./img/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script type='text/javascript' href='./js/loader.min.js'></script>
+    <link href="https://fonts.googleapis.com/css?family=Bungee+Hairline|Raleway|Unica+One&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css?family=Staatliches" rel="stylesheet">
+    <script src="//cdn.rawgit.com/mrdoob/three.js/master/build/three.min.js"></script>
+    <script src="three.js\build\three.js"></script>
+</head>
+<body class="web dark" id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+
+
+
+
